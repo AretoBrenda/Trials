b29ad04ac6bf6b5f9b59b0f56dd6d2660d6fa060
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-07-12 14:19:34 +0300

    Create AddtoAarrraylist.java

---
+++ AddtoAarrraylist.java
@@ -0,0 +1,16 @@
+//The ArrayList class has many useful methods. For example, to add elements to the ArrayList, use the add() method:
+
+import java.util.ArrayList;
+
+public class Main { 
+  public static void main( String[] args) {
+    
+    ArrayList<String> cars = new ArrayList<String>()
+    ;
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Mazda");
+    cars.add("Mark2");
+    System.out.println(cars);
+  }
+  }

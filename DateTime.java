ece9253d5323b8b4eed7a0afc145cf25a5faa6b2
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-07-12 14:00:10 +0300

    Create DateTime.java

---
+++ DateTime.java
@@ -0,0 +1,10 @@
+//Display Current Date and Time
+
+import java.time.LocalDateTime;  // import the LocalDateTime class
+
+public class Main {  
+  public static void main(String[] args) {  
+    LocalDateTime myObj = LocalDateTime.now();
+    System.out.println(myObj);
+  }  
+}  

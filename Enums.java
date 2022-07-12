52fa7299f12d6b36283a60d43e8d33a55ef8106d
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-07-12 12:56:13 +0300

    Create Enums.java

---
+++ Enums.java
@@ -0,0 +1,15 @@
+//You can also have an enum inside a class:
+
+public class Main {
+enum Level {
+LOW,
+MEDIUM,
+HIGH
+}
+public static void main(String[] args) {
+  
+Level myVar = Level.MEDIUM;
+
+System.out.println(myVar);
+}
+}

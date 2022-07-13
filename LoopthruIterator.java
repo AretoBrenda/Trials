c4369c4e4ec9e00a09973caa345ab751e6dccdfd
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-07-13 14:30:46 +0300

    Create LoopthruIterator.java

---
+++ LoopthruIterator.java
@@ -0,0 +1,23 @@
+//To loop through a collection, use the hasNext() and next() methods of the Iteratorbwith a while loop
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main {
+  public static void main(String[] args) {
+    
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+  
+    Iterator<String> it = cars.iterator();
+    
+    
+    while(it.hasNext()) {
+      System.out.println(it.next());
+    }
+  }
+}

25b5261400df7e17565731d2468699c3c9ac26f0
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-07-13 14:58:20 +0300

    Create WrapperToString.java

---
+++ WrapperToString.java
@@ -0,0 +1,10 @@
+//In the following example, we convert an Integer to a String, and use the length() 
+//method of the String class to output the length of the "string":
+
+public class Main { 
+  public static void main(String[] args) { 
+    Integer myInt = 100; 
+    String myString = myInt.toString();
+    System.out.println(myString.length());
+  }
+}

94ffa8ca7fee4c09fc0c6e47c673d179c2545eea
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-07-14 13:45:13 +0300

    Create Concurrancy.java

---
+++ Concurrancy.java
@@ -0,0 +1,20 @@
+//A code example where the value of the variable amount is unpredictable
+
+
+public class Main extends Thread {
+  public static int amount = 0;
+
+  public static void main(String[] args) {
+    Main thread = new Main();
+    thread.start();
+    System.out.println(amount);
+    amount++;
+    System.out.println(amount);
+  }
+
+  public void run() {
+    amount++;
+  }
+}
+
+
